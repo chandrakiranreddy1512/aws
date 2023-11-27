597da0dfbd6a89d1bdd89ac7ec9350e62e0871f4
chandrakiranreddy1512 <151384177+chandrakiranreddy1512@users.noreply.github.com>
2023-11-27 17:30:32 +0530

    Ec2instance.tf
    Hello,
    This is code to create Aws Ec2 Instance using Terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
     region = "us-east-1"
     #selected virginia region 
-    access_key = "AKIARLROB23Q4ORYL2PD"
-    secret_key = "Yyn0Lxkvfa8uJGcYwQIHejEcAceh/ih7SzCv26Hn"
+    access_key = "your_acces_key"
+    secret_key = "you_secret_key"
 }
 resource "aws_instance" "myoner" {
-  ami           = "ami-0fe8bec493a81c7da"
+  ami           = "ami-*******"
   instance_type = "t2.micro"
   #t2.micro is basic and only it can be used for free tier account 
 }
