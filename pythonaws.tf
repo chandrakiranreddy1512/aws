6ed54c7c9ea6d4d34b871f033b8636c40329d4b3
chandrakiranreddy1512 <151384177+chandrakiranreddy1512@users.noreply.github.com>
2023-11-27 17:43:23 +0530

    jupyterhub_Aws.tf

--- pythonaws.tf
+++ pythonaws.tf
@@ -1,17 +1,24 @@
 provider "aws" {
     region = "us-east-1" 
-    access_key = "AKIARLROB23QTXVMNTGM"
-    secret_key = "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"
+    access_key = "**************"
+    secret_key = "***********************"
 }
-resource "aws_instance" "juppyto" {
-  ami           = "ami-0fc5d935ebf8bc3bc"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ "sg-05fa22e3aa057142e" ]
-  key_name = "keytwo"
-  user_data = <<-EOF
+resource "aws_instance" "jupyterhub" {
+
+#here we have to select Ubuntu and enter the AMI ID
+ami           = "ami-*************"
+
+#instance type t2.small or t2.medium could be better 
+instance_type = "t2.micro"
+
+#here in the security group we have to allow HTTP and HTTPS
+  vpc_security_group_ids = [ "sg-***********" ]
+  key_name = "key_name_here"
+  
+user_data = <<-EOF
                     #!/bin/bash
                     curl -L https://tljh.jupyter.org/bootstrap.py \
                     | sudo python3 - \
-                    --admin chandra
+                    --admin name_here
                 EOF
 }
