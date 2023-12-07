77c879fc60a378d2c7b1f567c1c450cfb3fd8e88
chandrakiranreddy1512 <151384177+chandrakiranreddy1512@users.noreply.github.com>
2023-12-07 16:17:11 +0530

    mainfile.tf
    here we created a VPC and subnet, in that subnet we created a Ec2 instance.
    here we used variables instead of directly mentioning region, ami_id and instance type.
    here we created terraform.tfvars file where it contains the region, ami_id and instance type. 
    here we considered region, ami_id and instance type as variables

--- variable/mainfile.tf
+++ variable/mainfile.tf
@@ -1,5 +1,8 @@
-#here we created terraform.tfvars file where it contains the region , ami_id and instance type. 
-#here we considered region , ami_id and instance type as variables
+#here we created a VPC and subnet, in that subnet we created a Ec2 instance.
+#here we used variables instead of directly mentioning region, ami_id and instance type.
+#here we created terraform.tfvars file where it contains the region, ami_id and instance type. 
+#here we considered region, ami_id and instance type as variables
+
 variable "region" {
   
 }
@@ -11,7 +14,7 @@ variable "instance_type" {
     
 }
 provider "aws" {
-##instead of entering region  , we just gave var.region
+##instead of entering region, we just gave var.region
   region = var.region
   access_key = "****************"
   secret_key = "************"
@@ -20,7 +23,7 @@ provider "aws" {
 resource "aws_instance" "variable_exampe" {
 #instead of entering ami_id , we just gave var.ami_id 
     ami = var.ami_id
-#instead of entering instance type , we just gave var.instance_type 
+#instead of entering instance type, we just gave var.instance_type 
     instance_type = var.instance_type
     subnet_id = aws_subnet.var-subnet.id
 }
@@ -33,7 +36,7 @@ resource "aws_vpc" "var_vpc" {
 
 #here new subnet will be created in that vpc
 resource "aws_subnet" "var-subnet" {
-#here we mentioned "aws_vpc.var_vpc.id" , it takes the newly created vpc (line number 29)
+#here we mentioned "aws_vpc.var_vpc.id" , it takes the newly created vpc (line number 32)
   vpc_id     = aws_vpc.var_vpc.id
   cidr_block = "10.10.1.0/24"
 }
